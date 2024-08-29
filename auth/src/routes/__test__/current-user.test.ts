a5596cf4cffa4f39a21ca546a05ec71454231028
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-08-29 16:46:52 +0100

    Adding tests to all routers

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Response with details about the current user", async () => {
+    const cookie = await global.signup();
+
+    const response = await request(app)
+        .post("/api/users/currentuser")
+        .set("Cookie", cookie!)
+        .send()
+        .expect(200);
+
+    expect(response.body.currentUser.email).toEqual("test@gmail.com");
+});
+
+it("Response with null if not authenticated", async () => {
+    const response = await request(app)
+        .post("/api/users/currentuser")
+        .send()
+        .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
+});
