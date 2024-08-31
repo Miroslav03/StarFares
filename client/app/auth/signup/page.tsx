c8fae034567b4a14cff8135a1ea1ace1c5d5e142
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-08-31 22:05:22 +0100

    Finish Sign up page

--- client/app/auth/signup/page.tsx
+++ client/app/auth/signup/page.tsx
@@ -1,32 +1,43 @@
 export default function SignUp() {
     return (
-        <div className="flex w-full justify-center items-center min-h-screen">
+        <div className="flex w-full justify-center items-center py-[15rem] ">
             <div className="flex flex-col">
-                <h1 className="font-semibold text-center">Create your account</h1>
+                <h1 className="font-semibold text-center mb-12 text-3xl ">
+                    Create your account
+                </h1>
                 <form action="">
                     <div className="flex flex-col gap-2">
-                        <label htmlFor="" className="text-sm text-[#444444]">Email:</label>
+                        <label htmlFor="" className="text-sm text-[#444444]">
+                            Email:
+                        </label>
                         <input
                             type="text"
                             name=""
                             id=""
-                            className="bg-[#f8f8f8]"
+                            className="bg-[#f8f8f8] h-[2.4rem] rounded-[.5rem] pl-4 focus:outline-none focus:ring-2 focus:ring-[#f2e817]"
+                            placeholder="john@email.com"
                         />
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="" className="text-sm text-[#444444]">Password</label>
+                    <div className="flex flex-col gap-2 mt-3">
+                        <label htmlFor="" className="text-sm text-[#444444]">
+                            Password
+                        </label>
                         <input
                             type="text"
                             name=""
                             id=""
-                            className="bg-[#f8f8f8]"
+                            className="bg-[#f8f8f8] h-[2.4rem] rounded-[.5rem] pl-4 focus:outline-none focus:ring-2 focus:ring-[#f2e817]"
+                            placeholder="********"
                         />
                     </div>
                     <div>
-                    <button className="bg-gradient-to-r from-[#2EA94E] to-[#07ed55] h-[2rem] w-full text-sm rounded-[.5rem] font-semibold ">
-                        SignUp
-                    </button>
-                    <h2>Don't have an account?<span>SIGN UP</span></h2>
+                        <button style={{ color: 'white' }} className="bg-gradient-to-r from-[#ff8d09] to-[#f2e817] h-[2.4rem] w-full text-sm rounded-[.5rem] font-semibold uppercase mt-6 ">
+                            Sign Up
+                        </button>
+                        <h2 className="mt-3 text-center">
+                            Have an account?
+                            <span className="text-[#f2e817] ml-2">SIGN UP</span>
+                        </h2>
                     </div>
                 </form>
             </div>
