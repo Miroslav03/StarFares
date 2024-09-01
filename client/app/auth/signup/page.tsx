cc31a3dd2a13f5b1cb37b87aed85a46136018792
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-09-01 17:55:57 +0100

    Add logic to sign up component

--- client/app/auth/signup/page.tsx
+++ client/app/auth/signup/page.tsx
@@ -1,37 +1,70 @@
+"use client";
+import { useState, FormEvent } from "react";
+import useRequest from "@/hooks/useRequest";
+
 export default function SignUp() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { doRequest, errors } = useRequest({
+        url: "/api/users/signup",
+        method: "post",
+        body: { email, password },
+    });
+    
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        doRequest();
+    };
+
     return (
         <div className="flex w-full justify-center items-center py-[15rem] ">
             <div className="flex flex-col">
                 <h1 className="font-semibold text-center mb-12 text-3xl ">
                     Create your account
                 </h1>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <div className="flex flex-col gap-2">
-                        <label htmlFor="" className="text-sm text-[#444444]">
+                        <label
+                            htmlFor="email"
+                            className="text-sm text-[#444444]"
+                        >
                             Email:
                         </label>
                         <input
-                            type="text"
-                            name=""
-                            id=""
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className="bg-[#f8f8f8] h-[2.4rem] rounded-[.5rem] pl-4 focus:outline-none focus:ring-2 focus:ring-[#f2e817]"
                             placeholder="john@email.com"
                         />
                     </div>
                     <div className="flex flex-col gap-2 mt-3">
-                        <label htmlFor="" className="text-sm text-[#444444]">
+                        <label
+                            htmlFor="password"
+                            className="text-sm text-[#444444]"
+                        >
                             Password
                         </label>
                         <input
-                            type="text"
-                            name=""
-                            id=""
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="bg-[#f8f8f8] h-[2.4rem] rounded-[.5rem] pl-4 focus:outline-none focus:ring-2 focus:ring-[#f2e817]"
                             placeholder="********"
                         />
                     </div>
+                    <div className="text-[#ff2c2c] text-center text-[0.8rem] mt-1 font-semibold">
+                    {errors}
+                    </div>
                     <div>
-                        <button style={{ color: 'white' }} className="bg-gradient-to-r from-[#ff8d09] to-[#f2e817] h-[2.4rem] w-full text-sm rounded-[.5rem] font-semibold uppercase mt-6 ">
+                        <button
+                            style={{ color: "white" }}
+                            className="bg-gradient-to-r from-[#ff8d09] to-[#f2e817] h-[2.4rem] w-full text-sm rounded-[.5rem] font-semibold uppercase mt-6 "
+                        >
                             Sign Up
                         </button>
                         <h2 className="mt-3 text-center">
