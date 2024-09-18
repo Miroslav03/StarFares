17257194fb75f3bc67525ef5fece7b3905f32c5d
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-09-18 15:59:26 +0100

    Create library with reusable code and adding it

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@starfares/common";
 import { User } from "../models/User";
-import { BadRequestError } from "../errors/bad-request-error";
 import { Password } from "../utils/password";
 import jwt from "jsonwebtoken";
 
