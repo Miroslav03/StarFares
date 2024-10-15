63956976bc54492d4e137b17910cd17fc0ec4b35
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-15 18:37:32 +0100

    Update new order service and ticket model

--- orders/src/models/Ticket.ts
+++ orders/src/models/Ticket.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import { OrderStatus } from "@starfares/common";
+import { Order } from "./Order";
 
 interface TicketAttrs {
     title: string;
@@ -8,6 +10,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
+    isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -38,6 +41,21 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
+};
+
+ticketSchema.methods.isReserved = async function () {
+    const existingOrder = await Order.findOne({
+        ticket: this,
+        status: {
+            $in: [
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete,
+                OrderStatus.Created,
+            ],
+        },
+    });
+
+    return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,58 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import {
+    BadRequestError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from "@starfares/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/Ticket";
+import { Order } from "../models/Order";
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-    /* const orders = await Ticket.find({}); */
+router.post(
+    "/api/orders",
+    requireAuth,
+    [
+        body("ticketId")
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage("TicketId must be provided"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { ticketId } = req.body;
 
-    /* res.send(orders); */
-});
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        const isReserved = await ticket.isReserved();
+        if (isReserved) {
+            throw new BadRequestError("Ticket is already reserved");
+        }
+
+        const expiration = new Date();
+        expiration.setSeconds(
+            expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS
+        );
+
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket,
+        });
+        await order.save();
+
+        res.status(201).send(order);
+    }
+);
 
 export { router as createOneOrdersRouter };
