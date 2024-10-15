97032bc8bd8d3fab1ba7085f22170a2457077327
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-15 08:40:02 +0100

    Adding nats env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,16 +9,31 @@ if (!process.env.JWT_KEY) {
 if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined!");
 }
+if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined!");
+}
+
+if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined!");
+}
+
+if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined!");
+}
 
 const start = async () => {
-    await natsWrapper.connect("ticketing", "das", "http://nats-srv:4222");
+    await natsWrapper.connect(
+        process.env.NATS_CLUSTER_ID!,
+        process.env.NATS_CLIENT_ID!,
+        process.env.NATS_URL!
+    );
     natsWrapper.client.on("close", () => {
         console.log("NATS connection closed!");
         process.exit();
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-    
+
     await mongoose
         .connect(process.env.MONGO_URI!)
         .then(() => {
