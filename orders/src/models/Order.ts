a421a44f5d9f6f1812d8597641024313ec48ce1f
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-15 19:19:04 +0100

    Add new ticket tests and small Ticket model fix

--- orders/src/models/Order.ts
+++ orders/src/models/Order.ts
@@ -22,7 +22,7 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
 
 const orderSchema = new mongoose.Schema(
     {
-        userID: {
+        userId: {
             type: String,
             required: true,
         },
