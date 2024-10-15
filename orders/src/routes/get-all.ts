f6200a9e5058cde8123218aca4166a2cf6d33dad
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-15 19:44:35 +0100

    Add get all orders test and get all orders route

--- orders/src/routes/get-all.ts
+++ orders/src/routes/get-all.ts
@@ -1,11 +1,15 @@
+import { currentUser, requireAuth } from "@starfares/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/Order";
 
 const router = express.Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-    /* const orders = await Ticket.find({}); */
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser!.id,
+    }).populate("ticket");
 
-    /* res.send(orders); */
+    res.send(orders);
 });
 
 export { router as getAllOrdersRouter };
