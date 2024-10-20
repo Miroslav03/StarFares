6a9270dd966bfc7cc28b2e114fa1b0e3e0b22c62
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-20 10:35:14 +0100

    Add order created event publisher to route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,6 +10,8 @@ import {
 import { body } from "express-validator";
 import { Ticket } from "../models/Ticket";
 import { Order } from "../models/Order";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
@@ -50,6 +52,17 @@ router.post(
             ticket,
         });
         await order.save();
+
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            userId: order.userId,
+            status: order.status,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price,
+            },
+        });
 
         res.status(201).send(order);
     }
