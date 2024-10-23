acd864ea385a9bf47dd59a1ddcad5563b55f0b14
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-23 10:16:18 +0100

    Make a small fix to update route

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { natsWrapper } from "../../nats-wrapper";
+import { Ticket } from "../../models/Ticket";
 
 it("returns a 404 if the provided id does not exist", async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -115,4 +116,28 @@ it("publishes an event", async () => {
         .expect(200);
 
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it("Cannot update ticket if reserved", async () => {
+    const cookie = global.signup();
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send({
+            title: "title",
+            price: 20,
+        });
+
+    const ticket = await Ticket.findById(response.body.id);
+    ticket?.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+    await ticket?.save();
+    
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set("Cookie", cookie)
+        .send({
+            title: "titles",
+            price: 10,
+        })
+        .expect(400);
 });
