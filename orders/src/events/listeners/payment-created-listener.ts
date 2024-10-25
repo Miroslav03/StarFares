9a5633405fb8691c0ee725e03f186888499d9e11
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-25 12:30:45 +0100

    Create a payment created listener and wire it to orders

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,28 @@
+import {
+    Listener,
+    NotFoundError,
+    OrderStatus,
+    PaymentCreatedEvent,
+    Subjects,
+} from "@starfares/common";
+import { queueGroupName } from "../../constants/queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/Order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+
+        order.set({ staus: OrderStatus.Complete });
+        await order.save();
+
+        msg.ack();
+    }
+}
